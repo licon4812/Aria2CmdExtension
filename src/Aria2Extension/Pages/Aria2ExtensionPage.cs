e8e77b21e672653153c9770ebb7df9589b999894
James Alickolli <jalickolli@gmail.com>
2025-04-08 09:16:57 +1000

    Extension now checks if Aria2 is installed or not

--- src/Aria2Extension/Pages/Aria2ExtensionPage.cs
+++ src/Aria2Extension/Pages/Aria2ExtensionPage.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
 using Aria2Extension.Commands;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
@@ -19,8 +20,39 @@ internal sealed partial class Aria2ExtensionPage : ListPage
 
     public override IListItem[] GetItems()
     {
-        return [
-            new ListItem(new InstallAria2Command()) { Title = "Install Aria2" }
+        if (!IsAria2Installed())
+        {
+            return
+            [
+                new ListItem(new InstallAria2Command()) { Title = "Install Aria2" }
+            ];
+        }
+        return
+        [
+            new ListItem(new NoOpCommand()) { Title = "TODO: Download using Aria2c" }
         ];
+    }
+
+    internal bool IsAria2Installed()
+    {
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "winget",
+            Arguments = "list aria2.aria2",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using var process = new Process();
+        process.StartInfo = startInfo;
+        process.Start();
+        var output = process.StandardOutput.ReadToEnd();
+        var error = process.StandardError.ReadToEnd();
+        process.WaitForExit();
+
+        // Check if the output contains "aria2.aria2"
+        return output.Contains("aria2.aria2");
     }
 }
